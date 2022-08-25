d50a287304b58861459bb36af41e81296994c505
Ace0x <59582360+Ace0x@users.noreply.github.com>
2022-08-24 17:46:49 -0700

    yes

--- extest01/src/MxSystem.js
+++ extest01/src/MxSystem.js
@@ -11,28 +11,28 @@ function invert_date(values)
 function grade_switch(values)
 {
     if(values <= 93)
-        return "A", 4.0;
+        return ["A", 4.0];
     else if(values <= 90)
-        return "A-", 3.7;
+        return ["A-", 3.7];
     else if(values <= 87)
-        return "B+", 3.3;
+        return ["B+", 3.3];
     else if(values <= 83)
-        return "B", 3.0;
+        return ["B", 3.0];
     else if(values <= 80)
-        return "B-", 2.7;
+        return ["B-", 2.7];
     else if(values <= 77)
-        return "C+",2.3;
+        return ["C+",2.3];
     else if(values <= 73)
-        return "C",2.0;
+        return ["C",2.0];
     else if(values <= 70)
-        return "C-",1.7;
+        return ["C-",1.7];
     else if(values <= 67)
-        return "D+",1.3;
+        return ["D+",1.3];
     else if(values <= 64)
-        return "D",1.0;
+        return ["D",1.0];
     else
-        return "E",0.7;
+        return ["E",0.7];
 } 
 function evaluate_entry(values){
-    return change_surname(values[0]), invert_date(values[1]), grade_switch(values[2]);
+    return [change_surname(values[0]), invert_date(values[1]), grade_switch(values[2])];
 }
