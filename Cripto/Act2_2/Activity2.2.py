b00329828b6b53b8b3bac7648320ea54180cc91a
Cabrera-git <a01025453@itesm.mx>
2022-08-28 16:37:06 -0500

    Arranged file for aesthetics

--- Cripto/Act2_2/Activity2.2.py
+++ Cripto/Act2_2/Activity2.2.py
@@ -4,101 +4,6 @@ from collections import Counter
 #global variable with usable alphabet
 alphabet = 'abcdefghijklmnopqrstuvwxyz '
 
-# Caesar cipher + decipher
-def CaesarCipher(p,k):
-    x = ""
-    for i in range(len(p)):
-        c = p[i]
-        if(c.isupper()):
-            x += chr((ord(c) + k - 65) % 26 + 65)
-        elif(c != " "):
-            x += chr((ord(c) + k - 97) % 26 + 97)
-        else:
-            x += c
-    return x
-
-
-""" print("Caesar Cipher:\n")
-x = (CaesarCipher("Hello World", 16));
-print(x)
-y = (CaesarCipher(x,26-16))
-print(y)  """
-
-# key generation for Vigenere ciphering
-def generateKey(p, k):
-    flag = False
-    k = list(k)
-    if len(p) == len(k):
-        flag = True
-    else:
-        for i in range(len(p) - len(k)):
-            k.append(k[i % len(k)])
-    for i in range(len(p)):
-            if(p[i].isupper()):
-                k[i] = k[i].upper()
-            else:
-                k[i] = k[i].lower()
-    return("" . join(k))
-
-# Vigenere cipher
-def VCipher(p, k):
-    x = ""
-    for i in range(len(p)):
-        c = p[i]
-        c2 = k[i]
-        if(c.isupper()):
-            y = chr((ord(c) + ord(c2) - 65) % 26 + 65)
-        elif(c != " "):
-            y = chr((ord(c) + ord(c2) - 97) % 26 + 97)
-        else:
-            y = c
-        x += y
-    return x
-
-# Vigenere decipher
-def DeVCipher(p, k):
-    x = ""
-    for i in range(len(p)):
-        c = p[i]
-        c2 = k[i]
-        if(c.isupper()):
-            y = chr((ord(c) - ord(c2) - 65) % 26 + 65)
-        elif(c != " "):
-            y = chr((ord(c) - ord(c2) - 97) % 26 + 97)
-        else:
-            y = c
-        x += y
-    return x
-
-""" print("\nVigenere Cipher:\n")
-k = generateKey("Hello World","YEET")
-print(k)
-x = (VCipher("Hello World", k));
-print(x)
-y = (DeVCipher(x,k));
-print(y)  """
-
-# one-time pad
-def OneTimePad(p):
-    k = ""
-    for i in range(len(p)):
-        x = 0
-        if(p[i].isupper()):
-            x = random.randint(65,90)
-            k += chr(x)
-        elif(p[i] != " "):
-            x = random.randint(97,122)
-            k += chr(x)
-        else:
-            k += p[i]
-    return [VCipher(p,k), k]
-
-""" print("\nOne Time Pad:\n")
-OTP = OneTimePad("Good Morning Chicago")
-print(OTP[0])
-print(OTP[1])
-print(DeVCipher(OTP[0],OTP[1])) """
-
 # file reader
 def fileReader(file):
     with open(file) as f:
@@ -190,3 +95,82 @@ def cipherTwo():
 
 cipherOne()
 cipherTwo()
+
+# ALTERNATE IMPLEMENTATIONS OF CIPHERS
+# --------------------------------------------------------------------------------
+
+# Caesar cipher + decipher
+def CaesarCipher(p,k):
+    x = ""
+    for i in range(len(p)):
+        c = p[i]
+        if(c.isupper()):
+            x += chr((ord(c) + k - 65) % 26 + 65)
+        elif(c != " "):
+            x += chr((ord(c) + k - 97) % 26 + 97)
+        else:
+            x += c
+    return x
+
+# key generation for Vigenere ciphering
+def generateKey(p, k):
+    flag = False
+    k = list(k)
+    if len(p) == len(k):
+        flag = True
+    else:
+        for i in range(len(p) - len(k)):
+            k.append(k[i % len(k)])
+    for i in range(len(p)):
+            if(p[i].isupper()):
+                k[i] = k[i].upper()
+            else:
+                k[i] = k[i].lower()
+    return("" . join(k))
+
+# Vigenere cipher
+def VCipher(p, k):
+    x = ""
+    for i in range(len(p)):
+        c = p[i]
+        c2 = k[i]
+        if(c.isupper()):
+            y = chr((ord(c) + ord(c2) - 65) % 26 + 65)
+        elif(c != " "):
+            y = chr((ord(c) + ord(c2) - 97) % 26 + 97)
+        else:
+            y = c
+        x += y
+    return x
+
+# Vigenere decipher
+def DeVCipher(p, k):
+    x = ""
+    for i in range(len(p)):
+        c = p[i]
+        c2 = k[i]
+        if(c.isupper()):
+            y = chr((ord(c) - ord(c2) - 65) % 26 + 65)
+        elif(c != " "):
+            y = chr((ord(c) - ord(c2) - 97) % 26 + 97)
+        else:
+            y = c
+        x += y
+    return x
+
+# one-time pad
+def OneTimePad(p):
+    k = ""
+    for i in range(len(p)):
+        x = 0
+        if(p[i].isupper()):
+            x = random.randint(65,90)
+            k += chr(x)
+        elif(p[i] != " "):
+            x = random.randint(97,122)
+            k += chr(x)
+        else:
+            k += p[i]
+    return [VCipher(p,k), k]
+
+# --------------------------------------------------------------------------------
