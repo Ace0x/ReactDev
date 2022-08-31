ee49cc7fc8778ea1eb8e7cdb3cc8bb13b544f64d
Cabrera-git <a01025453@itesm.mx>
2022-08-31 18:02:38 -0500

    a mongo s

--- Cripto/NodeExample1/index.js
+++ Cripto/NodeExample1/index.js
@@ -3,11 +3,21 @@ const fs = require("fs")
 const bodyParser = require("body-parser")
 const multer = require("multer")
 const crypto = require("crypto")
+const {MongoClient} = require("mongodb")
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}))
 app.set("view-engine", "ejs")
 const uploads = multer({dest:".temp"})
+
+let db;
+
+function connectToDb() {
+    let client = new MongoClient("mongodb://localhost/casos");
+    client.connect();
+    console.log("Conectado a la DB");
+    db = client.db();
+}
 
 app.get("/descargar", (req, res)=>{
     let files = fs.readdirSync(__dirname+"/.storage");
@@ -35,18 +45,25 @@ app.get("/cargar", (req, res)=> {
 })
 
 app.post("/cargar", uploads.single("archivo"), (req, res)=> {
+    let rutaDefinitiva = "/.storage/"+req.body.nombre;
     let inputFS = fs.createReadStream(__dirname+"/.temp/"+req.file.filename)
-    let outputFS = fs.createWriteStream(__dirname+"/.storage/"+req.body.nombre)
+    let outputFS = fs.createWriteStream(__dirname+rutaDefinitiva)
     let key = "abcabcabcabcabcabcabcabcabcabc12"
     let iv = "abcabcabcabcabc1"
     let cipher = crypto.createCipheriv("aes-256-cbc",key,iv)
     inputFS.pipe(cipher).pipe(outputFS)
     outputFS.on("finish", ()=> {
         fs.unlinkSync(__dirname+"/.temp/"+req.file.filename)
+        let aInsertar = {nombre:req.body.nombre, archivo:rutaDefinitiva}
+        db.collection("carpeta").insertOne(aInsertar, (err, res)=> {
+            if (err) throw err;
+            console.log(res);
+        })
     })
     res.render("cargar.ejs")
 })
 
 app.listen(1337, ()=>{
+    connectToDb();
     console.log("Server started in port 1337.....")
 })
